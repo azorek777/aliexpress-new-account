9b4145a57c7a6dae720d0deafc1f2b4d23378687
azorek777 <57835812+azorek777@users.noreply.github.com>
2021-04-29 17:22:38 +0200

    Update aliexpress bot.py
    fixed

--- aliexpress bot.py
+++ aliexpress bot.py
@@ -17,18 +17,18 @@ coupon_exist = len(coupon)
 if coupon_exist > 0:
     browser.find_element_by_class_name('btn-close').click()
     time.sleep(2)
-
 # Registry button
 browser.find_element_by_class_name('user-account-port').click()
 browser.find_element_by_class_name('join-btn').click()
 time.sleep(2)
-browser.find_element_by_class_name('fm-switch-action').click()
-time.sleep(1)
 # Checking if aliexpress suggest to login by google
 google = browser.find_elements_by_class_name('batman-channel-image')
 logintype = len(google)
 if logintype > 0:
     browser.find_element_by_css_selector('span.batman-channel-other').click()
+# Registry by email
+browser.find_element_by_class_name('fm-switch-action').click()
+time.sleep(1)
 # Registry
 login = browser.find_element_by_xpath('//input[@placeholder="Email address"]')
 login.send_keys(ali_log)
@@ -36,4 +36,14 @@ password = browser.find_element_by_xpath('//input[@placeholder="Password"]')
 password.send_keys(ali_pass)
 time.sleep(2)
 browser.find_element_by_class_name('fm-button').click()
-time.sleep(3)
+time.sleep(5)
+# Checking success of registry
+success = browser.find_elements_by_class_name('_4N5wQ')
+success_check = len(success)
+if success_check > 0:
+    print('Account created successful')
+# Countdown to exit
+for time_left in range(5):
+    print('Program will be closed in: ', 5 - time_left, 's')
+    time.sleep(1)
+browser.quit()
