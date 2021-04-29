c6fb8023737ab520306499999e28624d997afadc
azorek777 <57835812+azorek777@users.noreply.github.com>
2021-04-29 15:38:02 +0200

    Update aliexpress bot.py

--- aliexpress bot.py
+++ aliexpress bot.py
@@ -11,14 +11,19 @@ browser = webdriver.Chrome(ChromeDriverManager().install())
 # Going to website
 browser.get('https://www.aliexpress.com/')
 
-# Switching-off new user coupon notify
-browser.find_element_by_class_name('btn-close').click()
-time.sleep(2)
+# Checking if aliexpress suggest to use new user coupon
+coupon = browser.find_elements_by_class_name('btn-close')
+coupon_exist = len(coupon)
+if coupon_exist > 0:
+    browser.find_element_by_class_name('btn-close').click()
+    time.sleep(2)
 
 # Registry button
 browser.find_element_by_class_name('user-account-port').click()
 browser.find_element_by_class_name('join-btn').click()
 time.sleep(2)
+browser.find_element_by_class_name('fm-switch-action').click()
+time.sleep(1)
 # Checking if aliexpress suggest to login by google
 google = browser.find_elements_by_class_name('batman-channel-image')
 logintype = len(google)
